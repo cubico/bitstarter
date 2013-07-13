38fee1533703588fabc14171c027cf51020d90f5
Toni <ubuntu@ip-172-31-21-110.us-west-2.compute.internal>
2013-07-13 10:10:43 +0000

    Read index.html

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
-
+var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World!by Cubico');
+  fs.readFile('index.html', function (err, data) {
+  	if (err) console.log(err);
+  });
+  response.send(data);
 });
 
 var port = process.env.PORT || 5000;
