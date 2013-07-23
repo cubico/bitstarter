6e2c1e6f070cb0c6b889944141e07f8a57ca3cd5
Toni <ubuntu@ip-172-31-21-110.us-west-2.compute.internal>
2013-07-23 23:00:58 +0000

    first version marketing

--- web.js
+++ web.js
@@ -1,12 +1,17 @@
 var express = require('express');
 var fs = require('fs');
-var app = express.createServer(express.logger());
+var app = express.createServer();
 
-app.get('/', function(req, res) {
-  	res.send(fs.readFileSync('./index.html','utf8'));
+app.configure(function(a,b){
+  express.logger()
+  app.use('/',express.static(__dirname + '/'));
 });
 
-var port = process.env.PORT || 5000;
+/*app.get('/', function(req, res) {
+  	res.send(fs.readFileSync('./index.html','utf8'));
+});*/
+
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
